d251235a5f0103d7bbb883cb9d5c918f7c5947ae
NickSpyker <nicolas.spijkerman@epitech.eu>
2022-05-27 17:13:00 +0200

    finish && shell tools

--- src/tree/exec.c
+++ src/tree/exec.c
@@ -56,11 +56,39 @@ void exec_pipe(builtin_t *builtin, tree_t *tree, env_t *list, char **env)
     closefd(fd);
 }
 
-static void do_and_stuff(char **env, tree_t *tree, env_t *list)
+static void exe_and_stuff(char **env, char **cmd, char *path, int *stat)
 {
-    printf("Yo !\n");
-    p_ntty(HEADER, list);
-    return;
+    pid_t pid = 0;
+    int wstatus = 0;
+
+    pid = fork();
+    if (pid == 0) {
+        execve(path, cmd, env);
+    } else
+        waitpid(pid, &wstatus, 0);
+    *stat = wstatus;
+}
+
+static int do_and_stuff(char **env, tree_t *tree, char *cmd, env_t *list)
+{
+    tree_t *ls = tree;
+    char **cmd_tab = NULL;
+    char *path = NULL;
+    static int stat = 0;
+
+    if (stat)
+        return 0;
+    if (cmd == NULL) {
+        for (; ls->sep != NULL; ls = ls->right)
+            do_and_stuff(env, tree, ls->left->cmd, list);
+        do_and_stuff(env, tree, ls->cmd, list);
+        stat = 0;
+        return 0;
+    }
+    cmd_tab = strsplit(cmd, " \t", false);
+    path = get_path(list, cmd_tab);
+    exe_and_stuff(env, cmd_tab, path, &stat);
+    return 0;
 }
 
 void exec_tree(builtin_t *builtin, env_t *list, char **env, tree_t *tree)
@@ -75,7 +103,7 @@ void exec_tree(builtin_t *builtin, env_t *list, char **env, tree_t *tree)
     if (tree->sep)
         redirection(tree);
     if (str_isequal(tree->sep, "&&", true))
-        return do_and_stuff(tree, env, list);
+        return do_and_stuff(env, tree, NULL, list) ? 0 : p_ntty(HEADER, list);
     if (!tree->sep && tree->cmd) {
         tree->cmd = get_alias(list, tree->cmd);
         if (!(builtin = get_builtin(tree->cmd)))
